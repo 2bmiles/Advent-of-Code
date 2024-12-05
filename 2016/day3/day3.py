ff0b86221f7409ec796624ffbec034e6cb797b1e
2bmiles <milesf010@gmail.com>
2024-12-04 21:21:15 -0600

    day 3

--- 2016/day3/day3.py
+++ 2016/day3/day3.py
@@ -1,6 +1,6 @@
 import input
 
-def parse_triangles(triangles):
+def parse_triangles_part_1(triangles):
     triangle_list = triangles.split("\n")
     side_lengths = []
     for triangle in triangle_list:
@@ -11,14 +11,43 @@ def parse_triangles(triangles):
         side_lengths.append(current_side_lengths)
     return side_lengths
 
+def parse_triangles_part_2(triangles):
+    lines = triangles.split("\n")
+    list_1 = []
+    list_2 = []
+    list_3 = []
+    for line in lines:
+        current_side = 1
+        for side in line.split(" "):
+            if side.isnumeric():
+                match current_side:
+                    case 1:
+                        list_1.append(int(side))
+                    case 2:
+                        list_2.append(int(side))
+                    case 3:
+                        list_3.append(int(side))
+                current_side += 1
+    list_1.extend(list_2)
+    list_1.extend(list_3)
+    return list_1
+
 def part_1():
-    triangles = parse_triangles(input.triangles)
-    possible_triangle_count = 0
+    triangles = parse_triangles_part_1(input.triangles)
+    valid_triangle_count = 0
     for triangle in triangles:
         triangle.sort()
         if triangle[0] + triangle[1] > triangle[2]:
-            possible_triangle_count += 1
-    return possible_triangle_count
+            valid_triangle_count += 1
+    return valid_triangle_count
 
+def part_2():
+    number_list = parse_triangles_part_2(input.triangles)
+    valid_triangle_count = 0
+    for i in range(len(number_list) // 3):
+        sides = sorted([number_list[i * 3], number_list[i * 3 + 1], number_list[i * 3 + 2]])
+        if sides[0] + sides[1] > sides[2]:
+            valid_triangle_count += 1
+    return valid_triangle_count
 
-print(part_1())
+print(part_2())
